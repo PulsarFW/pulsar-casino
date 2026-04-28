87c56aaabee420188b3a22787a09b49075a93dc5
Artmines <arturowells@gmail.com>
2026-04-27 21:46:57 -0230

    feat(init):Initial version

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version("cerulean")
+games({ "gta5" })
+lua54("yes")
+client_script("@pulsar-core/exports/cl_error.lua")
+client_script("@pulsar-pwnzor/client/check.lua")
+
+author("Dr Nick")
+version("v1.0.0")
+
+client_scripts({
+	"client/**/*.lua",
+})
+
+server_scripts({
+	"config/**/*.lua",
+	"server/**/*.lua",
+})
+
+shared_scripts({
+	"shared/**/*.lua",
+})
